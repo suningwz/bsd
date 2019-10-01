ac7f45072515a4abcbc46a30c03e0897bca52f61
Valentin Thirion <fw@abakus.be>
2019-10-01 13:37:09 +0000

    [ADD] sale order partner selection improvement

--- product_bsd_price_management/__manifest__.py
+++ product_bsd_price_management/__manifest__.py
@@ -4,7 +4,21 @@
 
     'summary': """
     """,
+    'description': """
+        Product BSD Price Management
 
+
+        This module adds a page in notebook in product.template form. This page contains lines of model product.sale.price.line.
+        
+        This model is created by this module and contains a lot of price and quantity informations useful for sellers.
+        
+        The modules also adds a field in uom model to link uom to a specific price.
+        
+        This link allows users to select the price they want through unit of measure.
+
+
+        This module has been developed by François WYAIME @ AbAKUS it-solutions.
+    """,
     'author': "ABAKUS IT-SOLUTIONS",
     'website': "http://www.abakusitsolutions.eu",
 
