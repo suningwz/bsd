35cff6a615ba6178a2eb809201692d26b3324c15
Valentin Thirion <valentinthirion@gmail.com>
2019-11-19 11:01:32 +0100

    [IMP] Refactor this file

--- product_disponibility/models/product_template.py
+++ product_disponibility/models/product_template.py
@@ -6,52 +6,38 @@ import datetime
 from datetime import timedelta
 from odoo.tools.float_utils import float_round
 
-class ProductTemplate( models.Model ):
+class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    date_disponibility = fields.Date( 
-        string    = _('Date mise à disposition'),
-        translate = True,
-        default   = "2050-01-01",
-        track_visibility = "onchange" 
-    )
+    date_disponibility = fields.Date(string=_('Date de mise à disposition'), default="2050-01-01", track_visibility="onchange")
 
-    def update_order_lines( self ):
-        SaleOrderLine = self.env[ 'sale.order.line' ]
-        order_line_ids = SaleOrderLine.search( 
-            [ ( 'product_id', '=', self.id ) ] )
+    def update_order_lines(self):
+        order_line_ids = self.env['sale.order.line'].search([('product_id', '=', self.id)])
 
         if not order_line_ids:
-            raise ValidationError( _( 'Ce produit n\'a aucune ligne de vente associée.' ) )
-
-
-        saleOrders = set()
+            raise ValidationError(_('Ce produit n\'a aucune ligne de vente associée.'))
 
         for line in order_line_ids:
             line.date_disponibility = self.date_disponibility
-            saleOrders.add( line.order_id )
-        
-        for order in saleOrders:
-            order.update_disponibility_date()
+            line.order_id.update_disponibility_date()
 
-    def action_view_sales( self ):
+    def action_view_sales(self):
         action = self.env.ref('product_disponibility.update_mad_date_action').read()[0]
         action['domain'] = [('product_tmpl_id', 'in', self.ids)]
         action['context'] = {
             'search_default_last_year': 1,
-            'search_default_team_id': 1
+            'search_default_team_id': 1,
         }
+
         return action
 
-class ProductProduct( models.Model ):
+class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    sales_count = fields.Float(
-        compute = "_compute_sales_count",
-        string  = "Vendu"
-    )
+    sales_count = fields.Float(compute="_compute_sales_count", string="Vendu(s)")
 
-    def _compute_sales_count( self ):
+    # TODO : check this method, the count returned is not correct
+    def _compute_sales_count(self ):
         r = {}
         if not self.user_has_groups('sales_team.group_sale_salesman'):
             return r
@@ -66,9 +52,10 @@ class ProductProduct( models.Model ):
             r[group['product_id'][0]] = group['product_uom_qty']
         for product in self:
             product.sales_count = float_round(r.get(product.id, 0), precision_rounding=product.uom_id.rounding)
+
         return r
 
-    def update_order_lines( self ):
+    def update_order_lines(self):
         return self.product_tmpl_id.update_order_lines()
 
     def action_view_sales(self):
@@ -78,4 +65,5 @@ class ProductProduct( models.Model ):
             'search_default_last_year': 1,
             'search_default_team_id': 1,
         }
+
         return action
