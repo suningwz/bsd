c25bfcef5eedf60997cfcb2c191bc3112b3f1fbe
Valentin Thirion <valentinthirion@gmail.com>
2019-11-19 10:54:26 +0100

    [IMP] Prepared README.RST + manifest

---
+++ product_disponibility/README.rst
@@ -0,0 +1,29 @@
+=====================================
+ Product Disponibility
+=====================================
+
+This module is a custom add-on to ``stock``, ``sale`` and ``purchase``.
+
+Managing disponibility dates
+
+Installation notes
+==================
+
+
+Credits
+=======
+
+Contributors
+------------
+* Psisoft (initial dev)
+* Valentin Thirion <valentin.thirion@abakusitsolutions.eu>
+* Francois Wyaime <francois.wyaime@abakusitsolutions.eu>
+
+Maintainer
+-----------
+
+.. image:: http://www.abakusitsolutions.eu/wp-content/themes/abakus/images/logo.gif
+   :alt: AbAKUS IT SOLUTIONS
+   :target: http://www.abakusitsolutions.eu
+
+This module is maintained by AbAKUS IT SOLUTIONS
--- product_disponibility/__manifest__.py
+++ product_disponibility/__manifest__.py
@@ -1,27 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Product Disponibility",
-
     'summary': """
         Managing disponibility dates""",
-
-    'description': """
-        Managing disponibility dates
-    """,
-
-    'author': "Psisoft",
+    'author': "Psisoft, ABAKUS- IT-SOLUTIONS",
     'website': "",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'website_sale', 'purchase', 'stock', 'product'],
-
-    # always loaded
+    'category': 'Sale',
+    'version': '0.2',
+    'depends': [
+        'sale',
+        'website_sale',
+        'purchase',
+        'stock',
+    ],
     'data': [
         # 'security/ir.model.access.csv',
         'views/product_template.xml',
@@ -30,8 +21,5 @@
         'views/stock_picking_views.xml',
         'views/update_mad_date_action.xml'
         #'views/scheduler.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
     ],
 }
